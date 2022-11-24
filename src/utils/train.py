c65d42f6d23f32466a4683a13b5f422641ca795d
IsitaRex <41337834+IsitaRex@users.noreply.github.com>
2022-11-24 00:50:52 -0500

    Update train.py

--- src/utils/train.py
+++ src/utils/train.py
@@ -2,8 +2,59 @@ import torch
 import wandb
 import torch.nn as nn
 import torch.nn.functional as F
-from src.models.lenet5 import LeNet5
-import datetime
+import numpy as np
+from datetime import datetime 
+from sklearn.metrics import roc_curve
+
+def get_roc_curve(model, data_loader, device):
+    '''
+    Function for computing the ROC curve of the predictions over the entire data_loader
+    '''
+    
+    y_true = []
+    y_prob = []
+    
+    with torch.no_grad():
+        model.eval()
+        for X, y in data_loader:
+
+            X = X.to(device)
+            y = y.to(device)
+
+            _, y_prob_batch = model(X)
+            y_prob.append(y_prob_batch[:,1].cpu().numpy())
+            y_true.append(y.cpu().numpy())
+
+    y_true = np.concatenate(y_true)
+    y_prob = np.concatenate(y_prob)
+    
+    
+    fpr, tpr, _ = roc_curve(y_true, y_prob)
+    
+    return fpr, tpr
+# from: https://towardsdatascience.com/implementing-yann-lecuns-lenet-5-in-pytorch-5e05a0911320
+def get_accuracy(model, data_loader, device):
+    '''
+    Function for computing the accuracy of the predictions over the entire data_loader
+    '''
+    
+    correct_pred = 0 
+    n = 0
+    
+    with torch.no_grad():
+        model.eval()
+        for X, y_true in data_loader:
+
+            X = X.to(device)
+            y_true = y_true.to(device)
+
+            _, y_prob = model(X)
+            _, predicted_labels = torch.max(y_prob, 1)
+
+            n += y_true.size(0)
+            correct_pred += (predicted_labels == y_true).sum()
+
+    return correct_pred.float() / n
 
 
 # from: https://towardsdatascience.com/implementing-yann-lecuns-lenet-5-in-pytorch-5e05a0911320
@@ -52,6 +103,7 @@ def validate(valid_loader, model, criterion, device):
         y_hat, _ = model(X) 
         loss = criterion(y_hat, y_true) 
         running_loss += loss.item() * X.size(0)
+        
 
     epoch_loss = running_loss / len(valid_loader.dataset)
         
@@ -66,7 +118,7 @@ def training_loop(model, criterion, optimizer, train_loader, valid_loader, epoch
     # set objects for storing metrics
     train_losses = []
     valid_losses = []
- 
+    
     # Train model
     for epoch in range(0, epochs):
 
@@ -81,17 +133,24 @@ def training_loop(model, criterion, optimizer, train_loader, valid_loader, epoch
 
         wandb.log({"train_loss": float(train_loss), "val_loss": float(valid_loss)}, step=epoch)
 
-        # if epoch % print_every == (print_every - 1):
+        if epoch % print_every == (print_every - 1):
             
-        #     train_acc = get_accuracy(model, train_loader, device=device)
-        #     valid_acc = get_accuracy(model, valid_loader, device=device)
+            train_acc = get_accuracy(model, train_loader, device=device)
+            valid_acc = get_accuracy(model, valid_loader, device=device)
                 
-        #     print(f'{datetime.now().time().replace(microsecond=0)} --- '
-        #           f'Epoch: {epoch}\t'
-        #           f'Train loss: {train_loss:.4f}\t'
-        #           f'Valid loss: {valid_loss:.4f}\t'
-        #           f'Train accuracy: {100 * train_acc:.2f}\t'
-        #           f'Valid accuracy: {100 * valid_acc:.2f}')
+            print(f'{datetime.now().time().replace(microsecond=0)} --- '
+                  f'Epoch: {epoch}\t'
+                  f'Train loss: {train_loss:.4f}\t'
+                  f'Valid loss: {valid_loss:.4f}\t'
+                  f'Train accuracy: {100 * train_acc:.2f}\t'
+                  f'Valid accuracy: {100 * valid_acc:.2f}')
+
+            wandb.log({"Train accuracy": float(100 * train_acc), "Valid accuracy": float(100 * valid_acc)}, step=epoch)
+            # get ROC curve
+            fpr, tpr = get_roc_curve(model, valid_loader, device)
+            wandb.log({"ROC curve": wandb.plot.line_series(fpr, tpr, title="ROC curve", xlabel="False positive rate", ylabel="True positive rate")}, step=epoch)
+           
+
 
     
     
