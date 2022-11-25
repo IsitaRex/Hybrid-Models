9c248d7f04cfcb162f72914661393b6707429316
IsitaRex <41337834+IsitaRex@users.noreply.github.com>
2022-11-24 19:06:49 -0500

    Update load_mnist.py

--- src/utils/load_mnist.py
+++ src/utils/load_mnist.py
@@ -1,4 +1,4 @@
-#import datasets from torch
+# import datasets from torch
 import torch
 import torchvision
 import torch.nn as nn
@@ -7,23 +7,32 @@ import torch.optim as optim
 from torch.utils.data import DataLoader
 from torchvision import datasets, transforms
 
-def load_mnist(batch_size):
+
+def load_mnist(batch_size, transform_type = "default"):
     """Load MNIST dataset.
-    
+
     Args:
         batch_size (int): Batch size.
-    
+
     Returns:
         trainloader: A DataLoader for iterating over the train set MNIST dataset.
         testloader: A DataLoader for iterating over the test set MNIST dataset.
     """
     # Define a transform to normalize the data
-    transform = transforms.Compose([transforms.Resize((32, 32)),
-                                 transforms.ToTensor()])
-    
-    trainset = datasets.MNIST('data', download=True, train=True, transform=transform)
-    trainloader = torch.utils.data.DataLoader(trainset, batch_size=batch_size, shuffle=True)
+    if transform_type == "default":
+        transform = transforms.Compose([transforms.ToTensor(),
+                                        transforms.Normalize((0.5,), (0.5,)),
+                                        ])
+    elif transform_type == "resize":
+        transform = transforms.Compose([transforms.Resize((32, 32)), transforms.ToTensor()])
 
-    testset = datasets.MNIST('data', download=True, train=False, transform=transform)
-    testloader = torch.utils.data.DataLoader(testset, batch_size=batch_size, shuffle=True)
+    trainset = datasets.MNIST("data", download=True, train=True, transform=transform)
+    trainloader = torch.utils.data.DataLoader(
+        trainset, batch_size=batch_size, shuffle=True
+    )
+
+    testset = datasets.MNIST("data", download=True, train=False, transform=transform)
+    testloader = torch.utils.data.DataLoader(
+        testset, batch_size=batch_size, shuffle=True
+    )
     return trainloader, testloader
