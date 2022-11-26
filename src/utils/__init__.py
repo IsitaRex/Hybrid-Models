992e198653ca6ae6699468114942dca2f615df2e
IsitaRex <41337834+IsitaRex@users.noreply.github.com>
2022-11-25 20:44:32 -0500

    Update __init__.py

--- src/utils/__init__.py
+++ src/utils/__init__.py
@@ -1,5 +1,6 @@
 from .load_mnist import load_mnist
 from .train_cnn import training_loop_cnn, predict
 from .train_gan import training_loop_gan
+from .train_autoencoder import training_loop_autoencoder
 from .wandb_setup import setup_wandb_logging
 from .tasks import task_cnn, task_gan, task_autoencoder
